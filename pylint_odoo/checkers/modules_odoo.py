08881c4727a2599105b917e447217c1cd6e181bc
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2022-10-20 13:19:30 -0500

    part3

--- pylint_odoo/checkers/modules_odoo.py
+++ pylint_odoo/checkers/modules_odoo.py
@@ -119,6 +119,7 @@ class ModuleChecker(misc.WrapperModuleChecker):
 
     @utils.check_messages('consider-merging-classes-inherited')
     def visit_assign(self, node):
+        import pdb;pdb.set_trace()
         if not self.odoo_node:
             return
         if not self.linter.is_message_enabled(
