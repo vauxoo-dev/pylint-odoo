35b22e2af8f58f710733787365478abcc08f9911
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2022-10-20 13:19:08 -0500

    part3

--- pylint_odoo/checkers/format.py
+++ pylint_odoo/checkers/format.py
@@ -9,30 +9,6 @@ from ..misc import PylintOdooTokenChecker
 ODOO_MSGS = {
     # C->convention R->refactor W->warning E->error F->fatal
 
-    'C%d01' % settings.BASE_FORMAT_ID: (
-        'No UTF-8 coding comment found: '
-        'Use `# coding: utf-8` or `# -*- coding: utf-8 -*-`',
-        'no-utf8-coding-comment',
-        settings.DESC_DFLT,
-        {"maxversion": (3, 0)},
-    ),
-    'C%d02' % settings.BASE_FORMAT_ID: (
-        'UTF-8 coding is not necessary',
-        'unnecessary-utf8-coding-comment',
-        settings.DESC_DFLT,
-        {"minversion": (3, 0)},
-    ),
-    'W%d01' % settings.BASE_FORMAT_ID: (
-        'You have a python file with execution permissions but you don\'t '
-        'have an interpreter magic comment, or a magic comment but no '
-        'execution permission. '
-        'If you really needs a execution permission then add a magic comment '
-        '( https://en.wikipedia.org/wiki/Shebang_(Unix) ). '
-        'If you don\'t needs a execution permission then remove it with: '
-        'chmod -x %s',
-        'incoherent-interpreter-exec-perm',
-        settings.DESC_DFLT
-    ),
     'W%d02' % settings.BASE_FORMAT_ID: (
         'Use of vim comment',
         'use-vim-comment',
@@ -51,28 +27,6 @@ class FormatChecker(PylintOdooTokenChecker):
 
     name = settings.CFG_SECTION
     msgs = ODOO_MSGS
-    odoo_check_versions = {
-        'no-utf8-coding-comment': {
-            'min_odoo_version': '4.2',
-            'max_odoo_version': '10.0',
-        },
-        'unnecessary-utf8-coding-comment': {
-            'min_odoo_version': '11.0',
-        },
-    }
-
-    def get_magic_comment_type(self, comment, line_num):
-        if line_num >= 1 and line_num <= 2:
-            if "#!" == comment[:2]:
-                return MAGIC_COMMENT_INTERPRETER
-            elif "# -*- coding: " in comment or "# coding: " in comment:
-                if "# -*- coding: utf-8 -*-" in comment \
-                   or "# coding: utf-8" in comment:
-                    return MAGIC_COMMENT_CODING_UTF8
-                return MAGIC_COMMENT_CODING
-            elif "# -*- encoding: " in comment:
-                return MAGIC_COMMENT_ENCODING
-        return NO_IDENTIFIED
 
     def is_vim_comment(self, comment):
         return True if comment.strip('# ').lower().startswith('vim:') \
@@ -92,17 +46,3 @@ class FormatChecker(PylintOdooTokenChecker):
                         token_content, line_num]
                 elif self.is_vim_comment(token_content):
                     self.add_message('use-vim-comment', line=line_num)
-        if not tokens_identified.get(MAGIC_COMMENT_CODING_UTF8) and \
-           not os.path.basename(self.linter.current_file) == '__init__.py':
-            self.add_message('no-utf8-coding-comment', line=1)
-        if (tokens_identified.get(MAGIC_COMMENT_CODING_UTF8)):
-            self.add_message('unnecessary-utf8-coding-comment', line=1)
-        access_x = os.access(self.linter.current_file, os.X_OK)
-        interpreter_content, line_num = tokens_identified.get(
-            MAGIC_COMMENT_INTERPRETER, ['', 0])
-        if (not platform.startswith('win') and
-                bool(interpreter_content) != access_x):
-            self.add_message(
-                'incoherent-interpreter-exec-perm',
-                line=line_num, args=(
-                    os.path.basename(self.linter.current_file)))
